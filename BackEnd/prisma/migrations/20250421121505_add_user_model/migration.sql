0c0fe7070cd17f7e17f41cdf8df0b113a6b112d6
Mukul Pal <mukulpal0333@gmail.com>
2025-04-21 17:53:06 +0530

    feat: setup Prisma client in db utility

---
+++ BackEnd/prisma/migrations/20250421121505_add_user_model/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "userRole" AS ENUM ('ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "image" TEXT,
+    "role" "userRole" NOT NULL DEFAULT 'USER',
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "verificationToken" TEXT,
+    "verificationTokenExpiry" TIMESTAMP(3),
+    "resetPasswordToken" TEXT,
+    "resetPasswordExpiry" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
