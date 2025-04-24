f4d50377a72a00e6f946b2f2d71cda1af1d059a6
Mukul Pal <mukulpal0333@gmail.com>
2025-04-24 20:44:19 +0530

    feat: add verify user endpoint

--- BackEnd/src/controllers/auth.controllers.js
+++ BackEnd/src/controllers/auth.controllers.js
@@ -25,7 +25,7 @@ const registerUser = async (req, res, next) => {
       email,
       password,
       verificationToken: token,
-      verificationTokenExpiry: new Date(Date.now() + 10 * 60 * 1000)
+      verificationTokenExpiry: new Date(Date.now() + 10 * 60 * 1000),
     },
   });
 
@@ -47,4 +47,39 @@ const registerUser = async (req, res, next) => {
   res.status(201).json(new ApiResponse(201, "User registered successfully"));
 };
 
-export { registerUser };
+const verifyUser = async (req, res, next) => {
+  const { token } = req.params;
+
+  if (!token) {
+    return next(new ApiError(400, "Token is missing"));
+  }
+
+  const user = await db.user.findFirst({
+    where: {
+      verificationToken: token,
+    },
+  });
+
+  if (!user) {
+    return next(new ApiError(404, "Invalid verification token"));
+  }
+
+  if (user.isVerified) {
+    return res.status(200).json({ message: "User already verified" });
+  }
+
+  if (user.verificationTokenExpiry < new Date()) {
+    return next(new ApiError(400, "Verification token has expired"));
+  }
+
+  await db.user.update({
+    where: { id: user.id },
+    data: {
+      isVerified: true,
+    },
+  });
+
+  res.status(200).json(new ApiResponse(200, "User verified successfully"));
+};
+
+export { registerUser, verifyUser };
--- BackEnd/src/routes/auth.routes.js
+++ BackEnd/src/routes/auth.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils/asynchandler.js";
-import { registerUser } from "../controllers/auth.controllers.js";
+import { registerUser, verifyUser } from "../controllers/auth.controllers.js";
+import { validateRegisterUser } from "../middlewares/uservalidator.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(asyncHandler(registerUser));
+router
+  .route("/register")
+  .post(validateRegisterUser, asyncHandler(registerUser));
+
+router.route("/verify/:token").post(asyncHandler(verifyUser));
 
 export default router;
