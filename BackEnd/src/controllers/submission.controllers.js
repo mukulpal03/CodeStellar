21b6fed632bb2277f5fcc0692af21ff279629191
Mukul Pal <mukulpal0333@gmail.com>
2025-05-08 00:12:57 +0530

    feat: add get submissions for problem functionality and update routes

--- BackEnd/src/controllers/submission.controllers.js
+++ BackEnd/src/controllers/submission.controllers.js
@@ -1,9 +1,43 @@
-const getSubmissions = async (req, res) => {
+import { db } from "../libs/db.js";
 
+const getSubmissions = async (req, res) => {
+  const userId = req.user.id;
+
+  const submissions = await db.submission.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  if (!submissions) {
+    throw new ApiError(404, "No submissions found");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Here are your submissions", submissions));
 };
 
 const getSubmissionById = async (req, res) => {
+  const { id } = req.params;
+  const userId = req.user.id;
 
+  const submission = await db.submission.findFirst({
+    where: {
+      id,
+      userId,
+    },
+  });
+
+  if (!submission) {
+    throw new ApiError(404, "No submission found");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Here is your submission", submission));
 };
 
-export { getSubmissions, getSubmissionById };
+const getSubmissionsForProblem = async (req, res) => {};
+
+export { getSubmissions, getSubmissionById, getSubmissionsForProblem };
--- BackEnd/src/routes/submission.routes.js
+++ BackEnd/src/routes/submission.routes.js
@@ -1,12 +1,20 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils/asynchandler.js";
 import { isLoggedIn } from "../middlewares/auth.middleware.js";
-import { getSubmissionById, getSubmissions } from "../controllers/submission.controllers.js";
+import {
+  getSubmissionById,
+  getSubmissions,
+  getSubmissionsForProblem,
+} from "../controllers/submission.controllers.js";
 
 const router = Router();
 
 router.route("/").get(isLoggedIn, asyncHandler(getSubmissions));
 
 router.route("/:id").get(isLoggedIn, asyncHandler(getSubmissionById));
+
+router
+  .route("/:problemId")
+  .get(isLoggedIn, asyncHandler(getSubmissionsForProblem));
 
 export default router;
