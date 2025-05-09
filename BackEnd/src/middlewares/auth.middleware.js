ffdffeb59888df999dd6ef757278991682e101fe
Mukul Pal <mukulpal0333@gmail.com>
2025-05-09 17:07:15 +0530

    refactor: simplify user retrieval in authentication middleware

--- BackEnd/src/middlewares/auth.middleware.js
+++ BackEnd/src/middlewares/auth.middleware.js
@@ -7,7 +7,7 @@ const isLoggedIn = async (req, _res, next) => {
     const accessToken = req.cookies?.accessToken;
 
     if (!accessToken) {
-      return next(new ApiError(401, "Unauthorized access"));
+      throw new ApiError(401, "Unauthorized access");
     }
 
     const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
@@ -16,48 +16,33 @@ const isLoggedIn = async (req, _res, next) => {
       where: {
         id: decoded.id,
       },
-      omit: {
-        password: true,
-        refreshToken: true,
-        verificationToken: true,
-        verificationTokenExpiry: true,
-        resetPasswordToken: true,
-        resetPasswordExpiry: true,
-      },
-    });
-
-    if (!user) {
-      return next(new ApiError(401, "Unauthorized access"));
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
-    return next(new ApiError(401, error?.message || "Invalid access token"));
-  }
-};
-
-const checkAdmin = async (req, res, next) => {
-  try {
-    const userId = req.user.id;
-
-    const user = await db.user.findUnique({
-      where: {
-        id: userId,
-      },
       select: {
         role: true,
       },
     });
 
+    if (!user) {
+      throw new ApiError(401, "Unauthorized access");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+};
+
+const checkAdmin = async (req, _res, next) => {
+  try {
+    const user = req.user;
+
     if (!user || user.role !== "ADMIN") {
-      return next(new ApiError(403, "You do not have permission"));
+      throw new ApiError(403, "You do not have permission");
     }
 
     next();
   } catch (error) {
-    console.log(error);
-    return next(new ApiError(500, "Error checking admin role"));
+    throw new ApiError(500, "Error checking admin role");
   }
 };
 
