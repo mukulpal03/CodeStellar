a80aea3c1a35c8b02ea9effb51165c3de9fd1dbe
Mukul Pal <mukulpal0333@gmail.com>
2025-05-20 22:13:24 +0530

    feat: integrate Tailwind CSS and remove legacy styles

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Welcome To CodeStellar</h1>
+      <h1 className="text-3xl font-bold text-center">Welcome To CodeStellar</h1>
     </>
   );
 }
